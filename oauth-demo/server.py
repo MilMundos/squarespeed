6d9e7e5cb52ba7f3392a48d5d3f671d95c45bce4
Cristina Muñoz <hi@xmunoz.com>
2021-09-24 16:25:09 -0500

    Format with black

--- oauth-demo/server.py
+++ oauth-demo/server.py
@@ -1,22 +1,22 @@
 import json, requests
 
 from urllib.parse import parse_qsl, urlencode
-from http.server  import HTTPServer, BaseHTTPRequestHandler
-from jinja2       import Environment, FileSystemLoader
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader
 
 HOST = "localhost"
 PORT = 8080
-ENV  = Environment(loader=FileSystemLoader("templates"))
+ENV = Environment(loader=FileSystemLoader("templates"))
 
 with open("config.json", "r") as file:
     CONFIG = json.load(file)
 
-class Server(BaseHTTPRequestHandler):
 
+class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
 
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-type", "text/html")
         self.end_headers()
 
         if "redirect" in self.path:
@@ -25,11 +25,14 @@ class Server(BaseHTTPRequestHandler):
             print(self.path)
 
         else:
-            template = ENV.get_template('index.html')
-            self.wfile.write(template.render(client_id = CONFIG["client_id"]).encode())
+            template = ENV.get_template("index.html")
+            self.wfile.write(template.render(client_id=CONFIG["client_id"]).encode())
+
 
 with HTTPServer((HOST, PORT), Server) as httpd:
-    print(f'starting server at {HOST}:{PORT}')
+    print(f"starting server at {HOST}:{PORT}")
 
-    try:                      httpd.serve_forever()
-    except KeyboardInterrupt: httpd.server_close()
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
